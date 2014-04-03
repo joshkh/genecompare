75fa573345d28b9581494c96bd323c9f2c68b57c
Joshua Heimbach <joshkh@gmail.com>
2014-04-03 19:08:42 +0100

    Added UI elements and mock data.

--- example/myApp.js
+++ example/myApp.js
@@ -18,51 +18,102 @@ myApp.filter('orderObjectBy', function(){
  }
 });
 
+myApp.service('Photo', ['$log', function ($log) {
+
+    var flymine   = new intermine.Service({root: 'www.flymine.org/query'});
+
+    oids = [19071979, 19071980, 19071987];
+
+
+
+
+    function getQuery(root, ids) {
+        var query = {
+        "from": root,
+        "select":["*"],
+        "where":
+                        [
+                            {"path":root,"op":"IN","ids":ids.slice()}
+                        ]
+                }
+        return query;
+    }
+
+    var types = ['Gene', 'ProteinDomain'];
+    for (i in types) {
+        var q = getQuery(types[i], oids);
+
+        flymine.records(q).then(function(rows) {
+            console.log("rows", rows);
+          console.log("No. of exons: " + rows.length);
+          rows.forEach(function printRow(row) {
+            console.log("[" + row[0] + "] " + row[1] + ":" + row[2] + ".." + row[3]);
+          });
+        });
+    }
+
+    return {};
+
+}]);
 
 
 
 
 myApp.controller('controller', ['$scope', '$http', function ($scope, $http) {
 
-    
-
-
-
     $scope.orderByAttribute = '';
 
 
-    $scope.stringifiedData = "hello"
-    
-    
+}]).controller('appController', ['$scope', '$http', 'Photo', function ($scope, $http, Photo) {
 
-    $scope.loadPeople = function() {
 
-      //   console.log("TEST");
+    $scope.$on('LOAD',function(){$scope.loading=true});
+    $scope.$on('UNLOAD',function(){$scope.loading=false});
 
-      //   $.ajax({
-      //     type: "GET",
-      //     url: "testdata.json",
-      //   })
-      // .then(function( data ) {
 
-      //   $scope.$apply(function () {
-      //       $scope.testData = data;
-      //       $scope.stringed = "";
+    $scope.getSimilarGenes = function(gene) {
 
-      //       for (each in data) {
-      //           $scope.stringed += (JSON.stringify(data[each]));
-      //       }
+        gene = gene.replace(/^\s+|\s+$/g,"").split(/\s*,\s*/);
 
-      //   });
-    $scope.$emit("LOAD");
-    $http.jsonp("http://www.filltext.com/?callback=JSON_CALLBACK&rows=5&name={firstName}&score={randomNumber|100}&delay=2").
-    success(function (data) {
-        $scope.testData = data
-        $scope.$emit("UNLOAD");
-    })
 
-        
-      // });
+        $scope.$emit("LOAD");
+
+        $scope.searchTerms = gene;
+
+        var config = {
+            params: {
+                'callback': "json_callback"
+            }
+        }
+
+
+        $http.get("testdata/mostsimilar.json", config, {}).success(function (data) {
+
+            console.log("FINISHED", data);
+            $scope.testData = data
+            $scope.$emit("UNLOAD");
+        });
+
+    };
+
+    $scope.getCommonItems = function(value) {
+
+        $scope.$emit("LOAD");
+
+        var config = {
+            params: {
+                'callback': "json_callback"
+            }
+        }
+
+
+        $http.get("testdata/commonitems.json", config, {}).success(function (data) {
+
+            console.log("FINISHED", value);
+            $scope.$emit("UNLOAD");
+            $scope.commonItems = data[value];
+            console.log("CommonItems: " + JSON.stringify($scope.commonItems, null, 2));
+        });
 
     };
 
@@ -71,30 +122,5 @@ myApp.controller('controller', ['$scope', '$http', function ($scope, $http) {
         $scope.$emit("UNLOAD");
     };
 
-
-}]).controller('appController', ['$scope', function ($scope) {
-    $scope.$on('LOAD',function(){$scope.loading=true});
-    $scope.$on('UNLOAD',function(){$scope.loading=false});
-}]).
-controller('urlController', function ($scope, $http) {
-    /*this controller uses the url as a single string*/
-    // $http.jsonp("http://www.filltext.com/?callback=JSON_CALLBACK&rows=5&name={firstName}&score={randomNumber|100}").
-    // success(function (data) {
-    //     $scope.users = data
-    // })
-}).
-controller('objController', function ($scope, $http) {
-    /*this controller uses the config.params object as the request payload*/
-    var config = {
-        params: {
-            'rows': 5,
-            'fname': '{name}',
-            'lname': '{score}',
-            'tel': '{phone|format}',
-        }
-    }
-    $http.jsonp("http://www.filltext.com", config, {}).success(function (data) {
-        $scope.users = data
-    });
-
-});
+    
+}]);
